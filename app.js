5a90a144e4c761a5a11b53dcb333f19be2a756ec
Ilya200918 <95967188+Ilya200918@users.noreply.github.com>
2023-02-04 19:40:34 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+function first() {
+    // Про 10+50 кнопка
+    alert(`Будет ${2+2}`)
+    }
+    function second() {
+    // prompt() - мы можем записать данные в переменную
+    let age = prompt('Введи своё имя: ')
+    }
+    function third() {
+    // Вставить в элемент HTML с id 'zp' вставить тысячу
+    document.getElementById('zp').innerHTML = '10000$';
+    }
+    
